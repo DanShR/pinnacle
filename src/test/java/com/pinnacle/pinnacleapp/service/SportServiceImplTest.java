32fb4a37c6ec19d8ea24171fabc68dbffbd077ce
Daniyar Shulakov <dshulakov@gmail.com>
2019-12-07 20:27:41 +0600

    simple test

---
+++ src/test/java/com/pinnacle/pinnacleapp/service/SportServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.pinnacle.pinnacleapp.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SportServiceImplTest {
+
+    @Test
+    void createSport() {
+        int i = 1;
+        int d = 7;
+        assertEquals(8, i + d);
+    }
+}
